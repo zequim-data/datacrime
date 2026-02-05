a4041ef6dfde95a072a328577886f21fc27c7584
Guilherme Zequim <guilherme@ZEQUIM.ZEQUIM>
2026-02-05 11:41:15 -0300

    feat: configurado deploy automático via GitHub Actions

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-echo "🗑️  Limpando cache do Flutter..."
-flutter clean
-flutter pub get
-
-echo "🏗️  Gerando NOVO build (Modo Moderno)..."
-flutter build web --release
-
-echo "🚀 Subindo para o Surge (Forçando atualização)..."
-cd build/web
-# O Surge não tem 'clear cache', então o segredo é garantir que o build/web esteja zerado antes
-surge . --domain datacrime.surge.sh
+flutter build web && firebase deploy --only hosting
